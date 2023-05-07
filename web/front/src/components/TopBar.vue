f3d6e6cd4b2d04109a28d205dec0198bbdc40656
Asakijz <bxp3315@gmail.com>
2023-05-07 13:50:34 +0800

    前端展示项目框架
    Took 2 seconds

---
+++ web/front/src/components/TopBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-app-bar app color="primary">
+            <v-avatar color="grey" size="40"> </v-avatar>
+            <v-container class="py-0 fill-height">
+                <v-btn text color="white">首页</v-btn>
+                <v-btn
+                    v-for="item in cateList"
+                    :key="item.id"
+                    text
+                    color="white"
+                    >{{ item.name }}</v-btn
+                >
+            </v-container>
+            <v-spacer></v-spacer>
+            <v-responsive max-width="260" color="white">
+                <v-text-field
+                    dense
+                    flat
+                    hide-details
+                    rounded
+                    solo-inverted
+                    dark
+                ></v-text-field>
+            </v-responsive>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cateList: []
+        }
+    },
+    created() {
+        this.getCateList()
+    },
+    methods: {
+        async getCateList() {
+            const { data: res } = await this.$http.get('category')
+            this.cateList = res.data
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ web/front/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app app>
+        <TopBar></TopBar>
+
+        <v-main>
+            <v-container></v-container>
+        </v-main>
+
+        <Footer></Footer>
+    </v-app>
+</template>
+
+<script>
+import TopBar from '@/components/TopBar.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+    name: 'Home',
+
+    components: { TopBar, Footer }
+}
+</script>
